64e071df647b14da6986bfe9777852aa99690319
LadyginVladimirIVT-19-1b <63785050+LadyginVladimirIVT-19-1b@users.noreply.github.com>
2020-04-17 16:39:19 +0600

    Add files via upload

---
+++ 7 сортировка массива/Сортировка массива итог/Сортировка массива итог.cpp
@@ -0,0 +1,176 @@
+﻿// Сортировка массива итог.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+// Отсортировать массив методами: вставки, пузырька и выбора
+
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void cout_mass(int mass[], int n) //Вывод массива
+{
+    int i = 0;
+    //Вывод массива
+    cout << "Шаг сортировки массива" << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << mass[i] << "\t";
+    }
+    cout << endl;
+}
+
+void insert(int mass[], int n, bool answer = false) //Метод вставки
+{
+    int p;
+    int i = 0;
+    int j = 0;
+    for (int i = 2; i <= n; i++)                    //Перебор всего массива
+    {
+        j = i;
+        answer = false;
+        while (j > 0 && answer == false)              //Сортировка массива
+        {
+            if (mass[j - 1] <= mass[j])
+            {
+                answer = true;
+            }
+            else                                    //Перестановка двух элементов с помощью дополнительной переменной
+            {
+                p = mass[j - 1];
+                mass[j - 1] = mass[j];
+                mass[j] = p;
+                j--;
+            }
+        }
+        //Вывод массива
+        cout_mass(mass, n);
+    }
+    cout << endl << endl;
+}
+
+
+void vial(int mass[], int n)     //Метод пузырька
+{
+    int p;
+    int i = 0;
+    int j = 0;
+    for (int i = 1; i <= n; i++)		// Перебор всех элементов массива
+    {
+        j = 1;
+        while (j <= n - i)			// Сортировка
+        {
+            if (mass[j] > mass[j + 1])
+            {
+                p = mass[j];
+                mass[j] = mass[j + 1];
+                mass[j + 1] = p;
+
+                //Вывод массива
+                cout_mass(mass, n);
+            }
+            else
+            {
+                j++;
+            }
+        }
+    }
+    cout << endl << endl;
+}
+
+
+void of_choice(int mass[], int n, int min)   //Метод выбора
+{
+    int p;
+    int i = 0;
+    int j = 0;
+    for (int i = 1; i <= n - 1; i++)		//Проходит все элементы массива и сравнивает их
+    {
+        min = mass[i];
+        p = i;
+        for (int j = i + 1; j <= n; j++)		//Соритровка массива
+        {
+            if (mass[j] < min)
+            {
+                min = mass[j];
+                p = j;
+            }
+        }
+        mass[p] = mass[i];
+        mass[i] = min;
+
+        //Вывод массива
+        cout_mass(mass, n);
+    }
+    cout << endl << endl;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(0));
+
+    int n = -1;
+    int i = 0;
+    int min = 0;
+    bool answer = false;
+    int start1 = 0;
+    int work = -1;
+
+    while (n <= 0)              //Длинна массива
+    {
+        cout << "Введите количество элементов массива" << endl;
+        cin >> n;
+    }
+    cout << endl;
+
+    //Ввод массива
+    cout << "Наш массив" << endl;
+    int* mass = new int[n];
+    for (int i = 1; i <= n; i++)
+    {
+        mass[i] = rand() % 101 - 50;
+        cout << mass[i] << "\t";
+    }
+    cout << endl << endl;
+
+    while ((work < 1) || (work > 3))
+    {
+        cout << "Введите с каким массивом будем работать: 1 - Вставка; 2 - Пузырьком; 3 - Выбора" << endl;
+        cin >> work;
+    }
+    cout << endl;
+
+
+    if (work == 1)
+    {
+        insert(mass, n, answer);
+    }
+    else if (work == 2)
+    {
+        vial(mass, n);
+    }
+    else
+    {
+        of_choice(mass, n, min);
+    }
+
+
+    //Вывод массива
+    cout << "Отсортированный массив" << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << mass[i] << "\t";
+    }
+    cout << endl;
+
+
+    clock_t end1 = clock();             //Время работы программы
+    double time1 = (double)(end1 - start1) / CLOCKS_PER_SEC;
+    cout << endl << "Затраченное время" << endl << time1;
+    cout << endl;
+
+}
+
---
+++ 8 массивы/Лаба4 (у Ольги Андреевны)/Лаба4 (у Ольги Андреевны).cpp
@@ -0,0 +1,182 @@
+﻿// Лаба4 (у Ольги Андреевны).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+/*Вариант№5: дан массив(int) с рандомайзером, выводим массив, удаляем все элементы массива кратные 7,
+после каждого нечетного элемента добавляетм элемент с 0, выводим что получилось*/
+
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+using namespace std;
+int n;
+int mass[100];
+int* pmass = new int[n];
+
+
+
+void introduce_stattic_mass()		//Ввод массива
+{
+	int i = 0;
+	for (int i = 0; i < n; i++)
+	{
+		mass[i] = rand() % 101 - 50;
+		cout << mass[i] << " ";
+	}
+}
+
+void change_mass()					//Удаление и добавление элемента
+{
+
+	int i = 0;
+	int j = 0;
+	while (i < n)
+	{
+		if (mass[i] == 0)														//Пропуск нуля
+		{
+			i++;
+		}
+		else if (abs(mass[i]) % 7 == 0)											//Уменьшение массива если элемент кратен 7
+		{
+			for (int j = i; j < n - 1; j++)									//Смещение массива влево 
+			{
+				mass[j] = mass[j + 1];
+			}
+			n--;
+		}
+		else if (abs(mass[i]) % 2 == 1)
+		{
+			for (int j = n; j >= i; j--)									//Увеличение массива и меняет элементы местами
+			{
+				mass[j + 1] = mass[j];
+			}
+			n++;
+			mass[i + 1] = 0;													//Перед нечетными элементами ставится 0		
+			i = i + 2;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	cout << endl;
+}
+
+void bring_out__static_mass()		//Вывод массива
+{
+	int i = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cout << mass[i] << " ";
+	}
+	cout << endl;
+}
+
+
+void introduce_dynamic_pmass()
+{
+	int i = 0;
+
+	//Ввод массива
+	for (int i = 0; i < n; i++)
+	{
+		pmass[i] = rand() % 101 - 50;
+		cout << pmass[i] << " ";
+	}
+}
+
+
+void change_pmass()
+{
+	int i = 0;
+	int j = 0;
+	while (i < n)
+	{
+		if (pmass[i] == 0)														//Пропуск нуля
+		{
+			i++;
+		}
+		else if (pmass[i] % 7 == 0)											//Уменьшение массива если элемент кратен 7
+		{
+			for (int j = i; j < n - 1; j++)									//Смещение массива влево 
+			{
+				pmass[j] = pmass[j + 1];
+			}
+			n--;
+		}
+		else if (abs(pmass[i]) % 2 == 1)									//После нечетными элементами ставится 0
+		{
+			for (int j = n; j >= i; j--)									//Увеличение массива и меняет элементы местами
+			{
+				pmass[j + 1] = pmass[j];
+			}
+			pmass[i + 1] = 0;													
+			i = i + 2;
+			n++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	cout << endl;
+}
+void bring_out__dynamic_pmass()
+{
+	int i = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cout << pmass[i] << " ";
+	}
+	cout << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, ("Russian"));
+	srand(time(NULL));
+
+	int work;
+
+	n = 0;
+	cout << "Введите какой массив будет, 0 - Статический, другое динамический." << endl;
+	cin >> work;
+
+	while (n <= 0)
+	{
+		cout << "Введите длину последовательности" << endl;
+		cin >> n;
+	}
+	cout << endl;
+
+
+	if (work == 0)
+	{
+		//Статический массви
+		int mass[100];
+
+		//Ввод массива
+		introduce_stattic_mass();
+
+		//Удаление и добавление элементов
+		change_mass();
+
+
+		//Ввывод массива
+		bring_out__static_mass();
+		
+	}
+	else
+	{
+		//Динамический массив
+
+		//Ввод массива
+		introduce_dynamic_pmass();
+
+		// Работа с массивом
+		change_pmass();
+
+		//Ввывод массива
+		bring_out__dynamic_pmass();
+		delete[] pmass;
+	}
+}
